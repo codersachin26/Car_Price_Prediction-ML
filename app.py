b8c25e2722427825e8f4555eee46c888e982ed9d
sachin <sachinindoriya63@gmail.com>
2021-01-11 16:26:46 +0530

    fix bugs

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask,render_template,request
+from datetime import datetime
 import pickle
-import sklearn
 
 
 app = Flask(__name__,template_folder='Templates')
-Model = pickle.load(open("Prediction_model.pkl","rb"))
+Model = pickle.load(open("Price_Prediction_Model.pkl","rb"))
 
 @app.route("/",methods=['POST','GET'])
 def price_prediction():
     if request.method == 'POST':
         Present_Price     = float(request.form['Present_Price'])
-        No_Year           = int(request.form['No_Year'])
+        Bought_Year           = int(request.form['Bought_Year'])
         Kms_Driven        = int(request.form['Kms_Driven'])
         Seller_Type       = request.form['Seller_Type']
         Transmission_Type = request.form['Transmission_Type']
         Fuel_Type         = request.form['Fuel_Type']
-
+        Current_Year = datetime.now().year
+        car = {
+            'present_price':Present_Price,
+            'bought_year': Bought_Year,
+            'total_kms':Kms_Driven,
+            'fuel_type':Fuel_Type,
+            'seller_type':Seller_Type,
+            'transmission_type':Transmission_Type
+        }
+        
         if(Seller_Type == 'Individual'):
             Seller_Type = 1
         else:
@@ -36,16 +45,17 @@ def price_prediction():
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=0 
 
-        No_Year = 2020 - No_Year
+        No_Year = Current_Year - Bought_Year
         Present_Price = Present_Price/100000                            
         data = [Present_Price,Kms_Driven,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type,Transmission_Type,No_Year]
         predicted_Value = Model.predict([data])
         selling_price = round(predicted_Value[0],2)
 
+
         if selling_price < 0:
             selling_price = None
             
-        return render_template('index.html',selling_price=selling_price)
+        return render_template('prediction.html',selling_price=selling_price,car=car)
     
     else:
         return render_template('index.html')
